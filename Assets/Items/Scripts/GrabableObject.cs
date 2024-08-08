89f9b5d0dd609e7c051d91797a6d92711f2d48f5
Edouard-2 <73876708+Edouard-2@users.noreply.github.com>
2024-08-08 17:28:42 +0200

    WIP

--- Assets/Items/Scripts/GrabableObject.cs
+++ Assets/Items/Scripts/GrabableObject.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 public class GrabableObject : ItemBase
 {
     [SerializeField] private SpringJoint _joint;
+    [SerializeField] private CustomEvent_String _updateName;
     [SerializeField] private Rigidbody _rb;
-
-
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        Name = "Active Grab";
     }
 
     public override void Use()
@@ -22,13 +22,16 @@ public class GrabableObject : ItemBase
             Name = "Cancel Grab";
             _joint.connectedBody = PlayerManager.Instance._data._rb;
             _joint.enablePreprocessing = true;
+            _joint.anchor = Vector3.zero;
+            _joint.connectedAnchor = Vector3.zero;
+            _updateName.Raise(Name);
+            return;
         }
-        else
-        {
-            Name = "Active Grab";
-            _joint.connectedBody = null;
-            _joint.enablePreprocessing = false;
-        }
+        Name = "Active Grab";
+        _joint.connectedBody = null;
+        _joint.enablePreprocessing = false;
+        _updateName.Raise(Name);
+
     }
     // button active grab
     // set joint from player to center object
