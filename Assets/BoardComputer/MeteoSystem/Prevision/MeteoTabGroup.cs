73512bd2ef97c1aac5957380910cc1db77f029a8
Narmalone <narmalone.game@gmail.com>
2024-09-05 15:25:25 +0200

    Update Ui Meteo tabs + début prévisions

---
+++ Assets/BoardComputer/MeteoSystem/Prevision/MeteoTabGroup.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoTabGroup : TabGroup
+{
+    private void Start()
+    {
+        
+    }
+
+    public void GetWeatherOf()
+    {
+
+    }
+}
