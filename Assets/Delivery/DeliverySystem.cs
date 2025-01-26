026057d1c9e38b9a9a514ad6108fef3096735f83
Votre Nom <narmalone.game@gmail.com>
2025-01-26 20:38:09 +0100

    Patch fix & Addressable System
    Fixs:
    -> quand on lachait les objets en dessous du joueur, ils traversaient parfois le sol
    
    Addressable Dictionnary
    
    Ajouts:
    -> Classe Custom qui ajoute des fonctionnalités Addressable comme load, instantiate, get...
    -> AddressableGroupSettings, il déssert directement l'utilité dans un editor afin de pré-stocker et générer des mots-clés pour instantier récupérer des assets / prefabs
    -> AddressableCustomUtility
    comprend d'autres fonctionnalités basiques sous forme de coroutine utilisiable y compris sans les settings
    
    Ajouts d'un vrai pluggin serialized dictionnary (l'autre contenait trop peu de support, objets, bugs ect...)

--- Assets/Delivery/DeliverySystem.cs
+++ Assets/Delivery/DeliverySystem.cs
@@ -1,21 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class DeliverySystem : Singleton<DeliverySystem>
 {
     public byte baseHoursToWait = 36;
     [SerializeField] private Transform SpawnPoint;
-
+    [SerializeField] private CustomEvent_Int _onDayChanged;
+    public AddressableLighthouseSettings _addressableLighthouseSettings;
     //chopper le minuit du prochain jour à l'aide du middnightX de weatherfordaysManager
     //Comparer les 2 résultats entre le minuit actuel et chopper la météo dans X heures
     //
-    public void GetSomething()
+    protected override void Awake()
     {
-        
+        base.Awake();
+        DayNightManager.OnDayChanged += OnDayChanged; 
     }
 
-    public void SpawnItem(ShopItemData item)
+    private void Start()
+    {
+    }
+
+    private void OnDestroy()
+    {
+        DayNightManager.OnDayChanged -= OnDayChanged;
+
+    }
+
+    private void OnDayChanged(int obj)
+    {
+        JerricanEssence ess = _addressableLighthouseSettings.InstantiatePrefabByAddress<JerricanEssence>(GeneratedKeys.Jerrican.ToString());
+        ess.transform.position = SpawnPoint.position;
+        /* StartCoroutine(AddressableCustomUtility.InstantiateAddressableCoroutine<JerricanEssence>(AddressableKeywords.JERRICAN_KEY, null, (s) =>
+         {
+             s.transform.position = SpawnPoint.position;
+         }));*/
+    }
+
+    public void SpawnItemFromShop(ShopItemData item)
     {
         if(item.Prefab == null) return;
         Instantiate(item.Prefab, SpawnPoint.position, Quaternion.identity);
--- Assets/Extensions/SerializedSample.cs
+++ Assets/Extensions/SerializedSample.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SerializedSample : MonoBehaviour
 {
-    public SerializableDictionary<string, List<MultiLanguage>> test;
+    //public SerializableDictionary<string, List<MultiLanguage>> test;
 
     private void Start()
     {
