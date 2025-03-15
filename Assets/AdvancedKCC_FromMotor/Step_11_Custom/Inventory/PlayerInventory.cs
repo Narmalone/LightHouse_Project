f1e57d6f5f240a056471cf628c99042c99e17a49
Votre Nom <narmalone.game@gmail.com>
2025-03-15 20:15:30 +0100

    suite du dernier commit

--- Assets/AdvancedKCC_FromMotor/Step_11_Custom/Inventory/PlayerInventory.cs
+++ Assets/AdvancedKCC_FromMotor/Step_11_Custom/Inventory/PlayerInventory.cs
@@ -1,23 +1,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using LightHouse.Interactions;
+using LightHouse.Inputs;
+using System.Linq;
 
 namespace LightHouse.Inventory
 {
     public class PlayerInventory : MonoBehaviour
     {
         private byte _inventoryCapacity = 4;
-        public Dictionary<IItem, GameObject> Inventory = new(); // Associe un IItem à son GameObject
+        private byte _currentSlotIndex = 0;
+        public List<IInventoryItem> Inventory = new(); // Associe un IItem à son GameObject
         [SerializeField] private Transform _inventoryParent = null;
         [SerializeField] private Transform _inventoryTarget = null;
         public static PlayerInventory Instance;
+        public LayerMask mask = 1 << -1;
+        public Camera _playerCamera;
 
         private void Awake()
         {
             Instance = this;
         }
 
-        public bool AddItem(IItem item)
+        public bool AddItem(IInventoryItem item)
         {
             if (Inventory.Count >= _inventoryCapacity)
             {
@@ -25,16 +30,19 @@ namespace LightHouse.Inventory
                 return false;
             }
 
-            GameObject obj = item.GetGameObject();
-            obj.SetActive(false); // Désactive l'objet dans la scène
+            GameObject obj = item.GetItem().gameObject;
+            //obj.SetActive(false); // Désactive l'objet dans la scène
             obj.transform.SetParent(_inventoryParent); // On le met en enfant de l'inventaire
+            obj.transform.localPosition = Vector3.zero;
+            obj.transform.localRotation = Quaternion.identity;
 
             item.GetCollider().enabled = false;
             item.HasRigidBody().isKinematic = true;
 
-            Inventory[item] = obj;
+            Inventory.Add(item);
+            item.OnItemAddedToInventory();
 
-            Debug.Log($"Objet ajouté : {item.GetName()}");
+            Debug.Log($"Objet ajouté : {item.GetItem().name}");
             return true;
         }
 
@@ -42,30 +50,94 @@ namespace LightHouse.Inventory
         {
             _inventoryParent.position = _inventoryTarget.position;
             _inventoryParent.rotation = _inventoryTarget.rotation;
-        }
 
-        public void RemoveItem(IItem item)
-        {
-            if (Inventory.ContainsKey(item))
+            Ray cameraRay = _playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0f));
+            TryInteratableItem(cameraRay, 3.0f, mask, QueryTriggerInteraction.Ignore);
+
+            if (InputManager.PLAYER_INPUTS_ACTIONS.Player.Drop.WasPerformedThisFrame())
             {
-                Inventory.Remove(item);
-                Debug.Log($"Objet retiré : {item.GetName()}");
+                //changer et faire en sorte de poser par rapport à la direction de la cam
+                //s'assurer qu'un objet ne puisse pas traverser le sol
+                //faire un système de puissance auquel si on maintien ça lance plus fort
+                DropItem(Inventory[0], _inventoryParent.transform.position + _inventoryParent.forward);
+                Debug.DrawRay(_inventoryParent.transform.position, _inventoryParent.forward, Color.green, 5f);
             }
         }
 
-        public void DropItem(IItem item, Vector3 dropPosition)
+        private GameObject _lastObjectSeen;
+        private IInventoryItem _inventoryItem;
+        private void TryInteratableItem(Ray ray, float interactionDistance, int interactableLayer, QueryTriggerInteraction qti)
         {
-            if (Inventory.ContainsKey(item))
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(ray, out hit, interactionDistance, interactableLayer, qti);
+
+            if (hasHit)
             {
-                GameObject obj = Inventory[item];
+                GameObject hitObject = hit.collider.gameObject;
+
+                //check if current hited object is different and apply it once
+                if (hitObject != _lastObjectSeen)
+                {
+                    _lastObjectSeen = hitObject;
+
+                    if (hitObject.GetComponent<IInventoryItem>() != null)
+                    {
+                        _inventoryItem = hitObject.GetComponent<IInventoryItem>();
+                    }
+                    Debug.Log(_inventoryItem.GetItem().name);
+                }
+
+                //If we are seing an object and performing our interact key we interacted with the item
+                if (_inventoryItem != null && InputManager.PLAYER_INPUTS_ACTIONS.Player.Pickup.WasPerformedThisFrame())
+                {
+                    AddItem(_inventoryItem);
+                    Debug.Log("Get item and add it to the inventory");
+                }
+            }
+            else
+            {
+                //Reset if we don't see anything
+                if (_lastObjectSeen != null)
+                {
+                    
+
+                    Debug.Log("Plus aucun objet interactif en vue.");
+                    _lastObjectSeen = null;
+
+                    if (_inventoryItem != null)
+                        _inventoryItem = null;
+                }
+            }
+
+            Debug.DrawRay(_playerCamera.transform.position, ray.direction * 3.0f, Color.cyan);
+        }
+
+        public void RemoveItem(IInventoryItem item)
+        {
+            if (Inventory.Contains(item))
+            {
+                Inventory.Remove(item);
+                item.OnItemRemovedFromInventory();
+                Debug.Log($"Objet retiré : {item.GetItem().name}");
+            }
+        }
+
+        //TO DO:: lancer des délégates / fonctions aux items lorsqu'on les drop / ajoutes à l'inv
+        public void DropItem(IInventoryItem item, Vector3 dropPosition)
+        {
+            if (Inventory.Contains(item))
+            {
+                GameObject obj = item.GetItem().gameObject;
 
                 obj.transform.SetParent(null); // Sort de l'inventaire
                 obj.transform.position = dropPosition;
                 obj.transform.rotation = Quaternion.identity;
+                item.GetCollider().enabled = true;
+                item.HasRigidBody().isKinematic = false;
                 obj.SetActive(true); // Réactive l’objet
 
                 RemoveItem(item); // Supprime du dictionnaire mais garde l'objet dans la scène
-                Debug.Log($"Objet droppé : {item.GetName()}");
+                Debug.Log($"Objet droppé : {item.GetItem().name}");
             }
         }
     }
