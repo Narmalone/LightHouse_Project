a10a8e542353f7dbb23ebcd2f0d69e23e650f9e6
Bru Thomas <narmalone.game@gmail.com>
2025-06-05 20:29:56 +0200

    WIP -> File Organisation

--- Assets/Application/WeatherSystem/Clouds/CloudTransitionSettings.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public struct CloudTransition
-{
-    public CloudSettings Clear;
-    public CloudSettings Stormy;
-    public AnimationCurve InfluenceCurve; // courbe pour affiner la transition selon l'humidité, pression, etc.
-}
