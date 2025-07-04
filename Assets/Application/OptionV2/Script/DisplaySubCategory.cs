2d26eeb8d3255966a7b2ff4d26cd75783ffcca03
Rehaxorde <liammamby@gmail.com>
2025-07-04 11:04:42 +0200

    personal toggle & enum codé

--- Assets/Application/OptionV2/Script/DisplaySubCategory.cs
+++ Assets/Application/OptionV2/Script/DisplaySubCategory.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class DisplaySubCategory : MonoBehaviour
 {
+    [SerializeField] private GameObject SubCategory;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
---
+++ Assets/Application/OptionV2/Script/ToggleParameter.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoolParameter : MonoBehaviour
+{
+    [SerializeField] private Button TrueButton;
+    [SerializeField] private Button FalseButton;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        FalseButton.image.color = new Color(1f, 1f, 1f, 0.5f);
+        TrueButton.image.color = new Color(1f, 1f, 1f, 1f);
+    }
+
+    public void OnClicTrue()
+    {
+        print("Vrai");
+        // change la couleur de l'autre boutton
+
+        FalseButton.image.color = new Color(1f, 1f, 1f,0.5f);
+
+        // change sa propre couleur
+        TrueButton.image.color = new Color(1f, 1f, 1f, 1f);
+
+        //Appeller une fonction lorsque l'interupteur est vrai
+    }
+    
+    public void OnClicFalse()
+    {
+        print("Faux");
+
+        // change la couleur de l'autre boutton
+        TrueButton.image.color = new Color(1f, 1f, 1f, 0.5f);
+
+        // change sa propre couleur
+        FalseButton.image.color = new Color(1f, 1f, 1f, 1f);
+
+        //Appeller une fonction lorsque l'interrupteur est faux
+    }
+}
