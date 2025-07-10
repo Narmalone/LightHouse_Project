9a5082b9b04dc7c6c9e61215ce9b6ce9a035d013
Bru Thomas <narmalone.game@gmail.com>
2025-07-10 15:38:07 +0200

    WIP -> NightWatch UI
    -> Mise en place de l'UI Nightwatch du PC

---
+++ Assets/Gameplay/Computer/OS/Applications/L.E.O/NightWatch/Scripts/NightWatchController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class NightWatchController : MonoBehaviour
+{
+    [SerializeField] private SonarUI _sonarUIController;
+    [SerializeField] private BuoysUI _buoysUIController;
+    
+
+    public void SwitchTo()
+    {
+
+    }
+}
