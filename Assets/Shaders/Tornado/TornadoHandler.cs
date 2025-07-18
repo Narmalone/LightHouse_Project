c802b2a937817de821ab30b6b2fd80b190264020
Slycott <63301247+Slycott@users.noreply.github.com>
2025-07-18 17:47:28 +0200

    TornadoControllerUpdate + ElectricArcs

--- Assets/Shaders/Tornado/TornadoHandler.cs
+++ Assets/Shaders/Tornado/TornadoHandler.cs
@@ -1,80 +1,169 @@
 using System;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.VFX;
 
 [ExecuteInEditMode]
 public class TornadoHandler : MonoBehaviour
 {
+    [Header("FogGameObjects")]
+    public GameObject GO_TornadoParticles;
+    public GameObject GO_BaseTornado;
+    public GameObject GO_SubTornado;
+    public GameObject GO_BottomFog;
+    public GameObject GO_TopFog;
+
+    
     [Header("Materials")]
-    public Material BaseTornadoMaterial;
-    public Material SubTornadoMaterial;
-    public Material BottomTornadoFogMaterial;
-    public Material TopTornadoFogMaterial;
-    public Material TornadoParticles;
+    public Material M_BaseTornadoMaterial;
+    public Material M_SubTornadoMaterial;
+    public Material M_BottomTornadoFogMaterial;
+    public Material M_TopTornadoFogMaterial;
+    public Material M_TornadoParticles;
 
-    private Material[] TornadoMaterials;
+    [Header("RenderDistance")]
+    public float RenderDistance = 10000;
 
-    [Header("RotationSettings")]
+    [Header("Shape")]
+    public float Height;
 
+    [Header("Rotation Settings")]
     public float RotationSpeed = 1f;
 
-    public float FogDistance = 15f;
 
+    [Header("Fog Distance")]
+    public float FogDistance = 15f;
+    [Range(0.01f, 2f)]
+    public float ParticlesFogDistanceDilution = 2f;
+
+
+    [Header("Colors")]
     public Color TornadoOuterColor;
     public Color TornadoInnerColor;
     public Color TornadoParticlesColor;
+    [Range(0,1)]
+    public float Opacity;
 
-    
+    private LocalVolumetricFog LVF_TornadoParticles;
+    private LocalVolumetricFog LVF_BaseTornado;
+    private LocalVolumetricFog LVF_SubTornado;
+    private LocalVolumetricFog LVF_BottomFog;
+    private LocalVolumetricFog LVF_TopFog;
+
     private static int _tornadoOuterColor = Shader.PropertyToID("_OuterColor");
     private static int _tornadoInnerColor = Shader.PropertyToID("_InnerColor");
+    private static int _tornadoOpacity = Shader.PropertyToID("_Opacity");
     private static int _tornadoParticlesColor = Shader.PropertyToID("_TornadoParticlesColor");
     private static int _tornadoRotationSpeed = Shader.PropertyToID("_RotationSpeed");
+    private static int _tornadoRadius = Shader.PropertyToID("_Radius");
+    private static int _tornadoRadiusOfGrowth = Shader.PropertyToID("_RadiusOfGrowth");
     private static int _tornadoFogDistance = Shader.PropertyToID("_FogVolumeFogDistanceProperty");
 
     void Start() 
     {
+        LVF_TornadoParticles = GO_TornadoParticles.GetComponent<LocalVolumetricFog>();
+        LVF_BaseTornado = GO_BaseTornado.GetComponent<LocalVolumetricFog>();
+        LVF_SubTornado = GO_SubTornado.GetComponent<LocalVolumetricFog>();
+        LVF_BottomFog = GO_BottomFog.GetComponent<LocalVolumetricFog>();
+        LVF_TopFog = GO_TopFog.GetComponent<LocalVolumetricFog>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        SetMaterials();
+        //DebugGetFogMaterialPropertyNames();
+        LVF_TornadoParticles = GO_TornadoParticles.GetComponent<LocalVolumetricFog>();
+        LVF_BaseTornado = GO_BaseTornado.GetComponent<LocalVolumetricFog>();
+        LVF_SubTornado = GO_SubTornado.GetComponent<LocalVolumetricFog>();
+        LVF_BottomFog = GO_BottomFog.GetComponent<LocalVolumetricFog>();
+        LVF_TopFog = GO_TopFog.GetComponent<LocalVolumetricFog>();
 
 
-        BaseTornadoMaterial.SetColor(_tornadoOuterColor,TornadoOuterColor);
-        SubTornadoMaterial.SetColor(_tornadoOuterColor, TornadoOuterColor);
-        TopTornadoFogMaterial.SetColor(_tornadoOuterColor, TornadoOuterColor);
-        BottomTornadoFogMaterial.SetColor(_tornadoOuterColor, TornadoOuterColor);
+        //Set positions
+        GO_TopFog.transform.localPosition = new Vector3(GO_TopFog.transform.localPosition.x,Height, GO_TopFog.transform.localPosition.z);
+        GO_BaseTornado.transform.localPosition = new Vector3(GO_TopFog.transform.localPosition.x, Height / 2, GO_TopFog.transform.localPosition.z);
+        GO_SubTornado.transform.localPosition = new Vector3(GO_TopFog.transform.localPosition.x, Height / 2, GO_TopFog.transform.localPosition.z);
+        GO_TornadoParticles.transform.localPosition = new Vector3(GO_TopFog.transform.localPosition.x, Height / 2, GO_TopFog.transform.localPosition.z);
 
+        //Update Local Volumetric Fogs
 
-        BaseTornadoMaterial.SetColor(_tornadoInnerColor, TornadoInnerColor);
-        SubTornadoMaterial.SetColor(_tornadoInnerColor, TornadoInnerColor);
-        TopTornadoFogMaterial.SetColor(_tornadoInnerColor, TornadoInnerColor);
-        BottomTornadoFogMaterial.SetColor(_tornadoInnerColor, TornadoInnerColor);
+        float updatedHeightForLVF = Height + Height / 5;
+        LVF_BaseTornado.parameters.size = new Vector3(updatedHeightForLVF / 2, updatedHeightForLVF, updatedHeightForLVF/2);
+        LVF_SubTornado.parameters.size = new Vector3(updatedHeightForLVF / 2, updatedHeightForLVF, updatedHeightForLVF / 2);
+        LVF_BottomFog.parameters.size = new Vector3(updatedHeightForLVF, Height/5, updatedHeightForLVF);
+        LVF_TopFog.parameters.size = new Vector3(updatedHeightForLVF, Height / 5, updatedHeightForLVF);
+        LVF_TornadoParticles.parameters.size = new Vector3(Height * 3 / 4, Height, Height * 3 / 4);
 
-        TornadoParticles.SetColor(_tornadoParticlesColor, TornadoParticlesColor);
+        LVF_BaseTornado.parameters.distanceFadeStart = RenderDistance;
+        LVF_SubTornado.parameters.distanceFadeStart = RenderDistance;
+        LVF_BottomFog.parameters.distanceFadeStart = RenderDistance;
+        LVF_TopFog.parameters.distanceFadeStart = RenderDistance;
+        LVF_TornadoParticles.parameters.distanceFadeStart = RenderDistance;
 
-        BaseTornadoMaterial.SetFloat(_tornadoRotationSpeed, RotationSpeed/2);
-        SubTornadoMaterial.SetFloat(_tornadoRotationSpeed, RotationSpeed/2);
-        TopTornadoFogMaterial.SetFloat(_tornadoRotationSpeed, -RotationSpeed*0.01f);
-        BottomTornadoFogMaterial.SetFloat(_tornadoRotationSpeed, RotationSpeed*0.01f);
-        TornadoParticles.SetFloat(_tornadoRotationSpeed, -RotationSpeed * 10);
+        LVF_BaseTornado.parameters.distanceFadeEnd = RenderDistance;
+        LVF_SubTornado.parameters.distanceFadeEnd = RenderDistance;
+        LVF_BottomFog.parameters.distanceFadeEnd = RenderDistance;
+        LVF_TopFog.parameters.distanceFadeEnd = RenderDistance;
+        LVF_TornadoParticles.parameters.distanceFadeEnd = RenderDistance;
 
-        BaseTornadoMaterial.SetFloat(_tornadoFogDistance, FogDistance/3);
-        SubTornadoMaterial.SetFloat(_tornadoFogDistance, FogDistance);
-        TopTornadoFogMaterial.SetFloat(_tornadoFogDistance, FogDistance);
-        BottomTornadoFogMaterial.SetFloat(_tornadoFogDistance, FogDistance);
-        TornadoParticles.SetFloat(_tornadoFogDistance, FogDistance/5f);
+    }
+    private void SetMaterials()
+    {
+        //Outer Color
+        M_BaseTornadoMaterial.SetColor(_tornadoOuterColor, TornadoOuterColor);
+        M_SubTornadoMaterial.SetColor(_tornadoOuterColor, TornadoOuterColor);
+        M_TopTornadoFogMaterial.SetColor(_tornadoOuterColor, TornadoOuterColor);
+        M_BottomTornadoFogMaterial.SetColor(_tornadoOuterColor, TornadoOuterColor);
 
-        // Search for fog volume property names
-        //Shader shader = BottomTornadoFogMaterial.shader;
-        //int propertyCount = shader.GetPropertyCount();
+        //Inner Color
+        M_BaseTornadoMaterial.SetColor(_tornadoInnerColor, TornadoInnerColor);
+        M_SubTornadoMaterial.SetColor(_tornadoInnerColor, TornadoInnerColor);
+        M_TopTornadoFogMaterial.SetColor(_tornadoInnerColor, TornadoInnerColor);
+        M_BottomTornadoFogMaterial.SetColor(_tornadoInnerColor, TornadoInnerColor);
 
-        //for (int i = 0; i < propertyCount; i++)
-        //{
-        //    string propertyName = shader.GetPropertyName(i);
-        //    Debug.Log("Property #" + i + ": " + propertyName + " (" + shader.GetPropertyType(i) + ")");
-        //}
+        //Particles Color
+        M_TornadoParticles.SetColor(_tornadoParticlesColor, TornadoParticlesColor);
 
+        //Rotation Speed
+        M_BaseTornadoMaterial.SetFloat(_tornadoRotationSpeed, RotationSpeed / 2);
+        M_SubTornadoMaterial.SetFloat(_tornadoRotationSpeed, RotationSpeed / 2);
+        M_TopTornadoFogMaterial.SetFloat(_tornadoRotationSpeed, -RotationSpeed * 0.01f);
+        M_BottomTornadoFogMaterial.SetFloat(_tornadoRotationSpeed, RotationSpeed * 0.01f);
+        M_TornadoParticles.SetFloat(_tornadoRotationSpeed, -RotationSpeed * 10);
+
+        //Fog Distance
+        M_BaseTornadoMaterial.SetFloat(_tornadoFogDistance, FogDistance / 3);
+        M_SubTornadoMaterial.SetFloat(_tornadoFogDistance, FogDistance);
+        M_TopTornadoFogMaterial.SetFloat(_tornadoFogDistance, FogDistance);
+        M_BottomTornadoFogMaterial.SetFloat(_tornadoFogDistance, FogDistance);
+        M_TornadoParticles.SetFloat(_tornadoFogDistance, FogDistance / (1 / ParticlesFogDistanceDilution));
+
+        //Opacity
+
+        M_BaseTornadoMaterial.SetFloat(_tornadoOpacity, Opacity);
+        M_SubTornadoMaterial.SetFloat(_tornadoOpacity, Opacity);
+        M_TopTornadoFogMaterial.SetFloat(_tornadoOpacity, Opacity);
+        M_BottomTornadoFogMaterial.SetFloat(_tornadoOpacity, Opacity);
+        M_TornadoParticles.SetFloat(_tornadoOpacity, Opacity);
+
+        //Radius
+        M_TopTornadoFogMaterial.SetFloat(_tornadoRadius, Height / 2);
+        M_TopTornadoFogMaterial.SetFloat(_tornadoRadiusOfGrowth, Height / 2);
+
+        M_BottomTornadoFogMaterial.SetFloat(_tornadoRadius, Height / 2);
+        M_BottomTornadoFogMaterial.SetFloat(_tornadoRadiusOfGrowth, Height / 2);
+    }
+    private void DebugGetFogMaterialPropertyNames()
+    {
+        Shader shader = M_BottomTornadoFogMaterial.shader;
+        int propertyCount = shader.GetPropertyCount();
+
+        for (int i = 0; i < propertyCount; i++)
+        {
+            string propertyName = shader.GetPropertyName(i);
+            Debug.Log("Property #" + i + ": " + propertyName + " (" + shader.GetPropertyType(i) + ")");
+        }
     }
 }
---
+++ Assets/VisualEffects/Electricity/ElectricArcController.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using UnityEngine.VFX;
+using System.Linq;
+using UnityEngine.VFX.Utility;
+using UnityEngine.UIElements;
+[ExecuteInEditMode]
+public class ElectricArcController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+
+    public VisualEffectAsset EffectAsset;
+    public int NumberOfSubArcs;
+    public GameObject Pos1;
+    public GameObject Pos2;
+    public GameObject Pos3;
+    public GameObject Pos4;
+
+
+    private static int _pos1 = Shader.PropertyToID("Pos1");
+    private static int _pos2 = Shader.PropertyToID("Pos2");
+    private static int _pos3 = Shader.PropertyToID("Pos3");
+    private static int _pos4 = Shader.PropertyToID("Pos4");
+    private static int _noiseFrequency = Shader.PropertyToID("NoiseFrequency");
+    private static int _noiseRange = Shader.PropertyToID("NoiseRange");
+    void Start()
+    {
+        for (int i = 0; i < NumberOfSubArcs; i++)
+        {
+            GameObject thisNewGameObject = new GameObject("Arc" + i);
+            thisNewGameObject.transform.parent = transform;
+            VisualEffect thisVisualEffectAsset = thisNewGameObject.AddComponent<VisualEffect>();
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    [ContextMenu("GenerateArcs")]
+    public void GenerateArcs()
+    {
+
+        foreach (Transform child in transform.Cast<Transform>().ToArray())
+        {
+            if (child.name.StartsWith("Arc"))
+            DestroyImmediate(child.gameObject);
+        }
+
+        for (int i = 1; i < NumberOfSubArcs+1; i++)
+        {
+            GameObject thisNewGameObject = new GameObject("Arc" + i);
+            thisNewGameObject.transform.parent = transform;
+            VisualEffect thisVisualEffectAsset = thisNewGameObject.AddComponent<VisualEffect>();
+            VFXPropertyBinder thisPropertyBinder = thisNewGameObject.AddComponent<VFXPropertyBinder>();
+
+
+
+
+            EditableVFXPositionBinder binder1 = thisPropertyBinder.AddPropertyBinder<EditableVFXPositionBinder>();
+            binder1.Target = Pos1.transform;
+            binder1.Property = "Pos1";
+            EditableVFXPositionBinder binder2 = thisPropertyBinder.AddPropertyBinder<EditableVFXPositionBinder>();
+            binder2.Target = Pos2.transform;
+            binder2.Property = "Pos2";
+            EditableVFXPositionBinder binder3 = thisPropertyBinder.AddPropertyBinder<EditableVFXPositionBinder>();
+            binder3.Target = Pos3.transform;
+            binder3.Property = "Pos3";
+            EditableVFXPositionBinder binder4 = thisPropertyBinder.AddPropertyBinder<EditableVFXPositionBinder>();
+            binder4.Target = Pos4.transform;
+            binder4.Property = "Pos4";
+
+
+
+            thisVisualEffectAsset.visualEffectAsset = EffectAsset;
+            thisVisualEffectAsset.SetVector3(_pos1, Pos1.transform.position);
+            thisVisualEffectAsset.SetVector3(_pos2, Pos2.transform.position);
+            thisVisualEffectAsset.SetVector3(_pos3, Pos3.transform.position);
+            thisVisualEffectAsset.SetVector3(_pos4, Pos4.transform.position);
+            thisVisualEffectAsset.SetFloat(_noiseRange, Random.Range(0,1f));
+            thisVisualEffectAsset.SetFloat(_noiseFrequency, Random.Range(0,1f));
+        }
+    }
+
+}
